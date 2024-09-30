f8df5f182759f673cc5ce7a4266176629e1f34d7
PrometheusDevvv <prometheus.devv@gmail.com>
2024-09-30 23:28:00 +0400

    ArithmeticOperators

---
+++ BroCode/7.ArithmeticOperators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BroCode
+{
+    internal class ArithmeticOperators
+    {
+        public ArithmeticOperators()
+        {
+            int friends = 5;
+            friends += 3;        // Increment friends by 3
+            friends -= 2;        // Decrement friends by 2
+            friends *= 2;        // Double the number of friends
+            friends /= 2;        // Halve the number of friends
+
+            int remainder = friends % 2; // Calculate remainder when friends is divided by 2
+
+            Console.WriteLine(friends);   // Output the total number of friends
+            Console.WriteLine(remainder);   // Output the remainder
+            Console.ReadKey();             // Wait for user input before closing
+        }
+    }
+}
--- BroCode/Program.cs
+++ BroCode/Program.cs
@@ -5,6 +5,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        UserInput output = new UserInput();
+        ArithmeticOperators output = new ArithmeticOperators();
     }
 }   
