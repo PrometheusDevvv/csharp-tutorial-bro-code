e4ef329a5d401b76b465368804d29c158b1dc750
PrometheusDevvv <prometheus.devv@gmail.com>
2024-10-01 13:36:06 +0400

    IfStatements

---
+++ BroCode/12.IfStatements.cs
@@ -0,0 +1,24 @@
+﻿namespace BroCode
+{
+    class IfStatements {
+        public IfStatements()
+        {
+            Console.Write("Please enter your name: ");
+            String name = Console.ReadLine();
+                
+            if (name !=  "")
+            {
+                Console.WriteLine($"Hello {name}");
+
+            }
+            else
+            {
+                Console.WriteLine("You didn\'t enter you name");
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- BroCode/Program.cs
+++ BroCode/Program.cs
@@ -5,6 +5,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        StringMethods output = new StringMethods();
+        IfStatements output = new IfStatements();
     }
 }   
