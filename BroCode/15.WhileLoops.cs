36cd4e81f9973b1d1564a49db5b7b04f37c1315f
PrometheusDevvv <prometheus.devv@gmail.com>
2024-10-01 14:32:56 +0400

    WhileLoops

---
+++ BroCode/15.WhileLoops.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BroCode
+{
+    internal class WhileLoops
+    {
+        public WhileLoops()
+        {
+            String name = "";
+            while (name == "") {
+                Console.Write("Enter Your Name: ");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
--- BroCode/Program.cs
+++ BroCode/Program.cs
@@ -5,6 +5,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        LogicalOperators output = new LogicalOperators();
+        WhileLoops output = new WhileLoops();
     }
 }   
