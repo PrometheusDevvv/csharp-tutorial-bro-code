4167c8327f2e90ec0df2c59f6e74210fa402dfa6
PrometheusDevvv <prometheus.devv@gmail.com>
2024-10-02 14:48:41 +0400

    ForeachLoop

---
+++ BroCode/22.ForeachLoop.cs
@@ -0,0 +1,18 @@
+﻿namespace BroCode
+{
+    internal class ForeachLoop
+    {
+        public ForeachLoop()
+        {
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            foreach(String car in cars)
+            {
+                Console.WriteLine(      );
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
